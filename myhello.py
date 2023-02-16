b3ef10bb869bf0c1d6ec927941d359517942d7a1
王川 <wangchuan7@xiaomi.com>
2023-02-16 09:58:08 +0800

    first commit

---
+++ myhello.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/2/16 9:53
+# @Author  : wangchuan7
+# @FileName: myhello.py
+# @Software: PyCharm
+print ("relay.xiaomi.com")
+print ("login.mioffice.cn")
+print ("b2c.srv")
